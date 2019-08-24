09de54e36e67a57ec136c890d2ed4fdcd2fa02c4
qitana <53415856+qitana@users.noreply.github.com>
2019-08-24 13:05:53 +0900

    add CAMERA

---
+++ Sharlayan/Models/ReadResults/CameraResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharlayan.Models.ReadResults
+{
+    using Sharlayan.Core;
+    public class CameraResult
+    {
+        public CameraInfo CameraInfo { get; set; } = new CameraInfo();
+    }
+}
---
+++ Sharlayan/Reader.Camera.cs
@@ -0,0 +1,57 @@
+﻿namespace Sharlayan
+{
+    using System;
+    using Sharlayan.Models.ReadResults;
+
+    static partial class Reader
+    {
+        public static bool CanGetCameraInfo()
+        {
+            var canRead = Scanner.Instance.Locations.ContainsKey(Signatures.CameraKey);
+            if (canRead)
+            {
+                // OTHER STUFF?
+            }
+
+            return canRead;
+        }
+
+        public static CameraResult GetCameraInfo()
+        {
+            var result = new CameraResult();
+
+            if (!CanGetCameraInfo() || !MemoryHandler.Instance.IsAttached)
+            {
+                return result;
+            }
+
+            try
+            {
+                var cameraAddress = (IntPtr)Scanner.Instance.Locations[Signatures.CameraKey];
+
+                if (cameraAddress.ToInt64() > 0)
+                {
+                    var heading = MemoryHandler.Instance.GetSingle(cameraAddress, MemoryHandler.Instance.Structures.CameraInfo.Heading);
+                    var elevation = MemoryHandler.Instance.GetSingle(cameraAddress, MemoryHandler.Instance.Structures.CameraInfo.Elevation);
+                    var mode = MemoryHandler.Instance.GetByte(cameraAddress, MemoryHandler.Instance.Structures.CameraInfo.Mode);
+
+                    result.CameraInfo.Heading = heading;
+                    result.CameraInfo.Elevation = elevation;
+                    result.CameraInfo.Mode = mode;
+
+                    if (Math.Abs(heading) <= Math.PI && Math.Abs(elevation) <= Math.PI)
+                    {
+                        result.CameraInfo.IsValid = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MemoryHandler.Instance.RaiseException(Logger, ex, true);
+            }
+
+            return result;
+        }
+
+    }
+}
